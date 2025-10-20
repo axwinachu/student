2cd2641d1f08906a029267c570f5a354d6bf4d32
axwinachu <ceaswin146@gmail.com>
2025-10-20 23:27:24 +0530

    Add files via upload

---
+++ maping/mapping2.js
@@ -0,0 +1,16 @@
+let obj={
+    name:"aswin",
+    product:{
+        ordres:[{
+    id:101,
+    name:"carrot"
+},{
+    id:102,
+    name:"carrot"
+},{
+    id:104,
+    name:"carrot"
+} ]
+    }
+}
+obj.product.ordres.map((d)=>console.log(d))
