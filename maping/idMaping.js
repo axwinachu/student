936ee6c1776b0baaf53162e6054fc0db12e1f948
axwinachu <ceaswin146@gmail.com>
2025-10-20 23:43:07 +0530

    EDITED
    Removed code that maps cart items to products.

--- maping/idMaping.js
+++ maping/idMaping.js
@@ -1,15 +1,1 @@
-let cart=[101,104]
-let product=[{
-    id:101,
-    name:"carrot"
-},{
-    id:102,
-    name:"carrot"
-},{
-    id:104,
-    name:"carrot"
-}]
-let data=cart.flatMap((data)=>product.filter((p)=>p.id==data))
-console.log(data)
-let data2=cart.map((c)=>product.find((p)=>p.id==c))
-console.log(data2)
+
